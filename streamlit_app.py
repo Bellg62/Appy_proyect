f62341d35bcecd146af29a239d3dac5630b7e450
Bellg62 <145160771+Bellg62@users.noreply.github.com>
2024-10-26 19:38:27 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,6 @@ if seleccion_menu == "Jefe de grupo":
                 selcar = pd.read_sql("SELECT DISTINCT Carrera FROM carreraalumn;", conect)
                 st.write("  \n")
                 selec_carrera= st.selectbox('Selecciona la carrera a la que perteneces:', selcar['Carrera'])
-                cursor = conect.cursor()
                 conect.close()
 
                #FUNCION PARA QUE PONGA LA ASISTENCIA
@@ -77,18 +76,19 @@ if seleccion_menu == "Jefe de grupo":
                         prof = pd.read_sql("SELECT DISTINCT Profesici FROM carreraalumn;", conect)
                         st.write("  \n")
                         selec_profesor = st.selectbox('Selecciona el profesor:', prof['Profesici'])
-                        s1= conect.cursor()
-                        s2 = conect.cursor()
-                        s3 = conect.cursor()
-                        s1.execute("SELECT * FROM profe WHERE Profesor=?",(selec_profesor,))
-                        s2.execute("SELECT FROM materiaprofeici WHERE Profesor=?",(selec_profesor,))
-                        s3.execute("SELECT FROM materiaprofe WHERE Profesor=? AND Asistencia=NULL",(selec_profesor,))
+                        cursor1= conect.cursor()
+                        cursor2 = conect.cursor()
+                        cursor3 = conect.cursor()
+                        cursor1.execute("SELECT * FROM profe WHERE Profesor=?",(selec_profesor,))
+                        cursor2.execute("SELECT FROM materiaprofeici WHERE Profesor=?",(selec_profesor,))
+                        cursor3.execute("SELECT FROM materiaprofeici WHERE Profesor=? AND Asistencia IS NULL",(selec_profesor,))
                         # Recuperar todos los registros
-                        profe_ici = s1.fetchall()
-                        matimparprofeici = s2.fetchall()
-                        Asistenciaprofeici = s3.fetchall()
+                        profe_ici = cursor1.fetchall()
+                        matimparprofeici = cursor2.fetchall()
+                        Asistenciaprofeici = cursor3.fetchall()
                         asistencia= st.number_input("¿Asistio el profesor? (Ingresa 1 si asistió y 0 si no asistió):",min_value=0, step=1 ,max_value=1)
                         st.write(f"Asistencia registrada para el profesor {profe_ici} que imparte {matimparprofeici}.")
+                        conect.close()
                                 
                       
                 
